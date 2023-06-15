63912220e564010dd0217107b2abccd837e86ce9
Aryan <aryanshroff21@gmail.com>
2023-06-15 11:20:32 +0530

    added movie rows

---
+++ src/Components/Row.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Row = ({ title, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios.get(fetchUrl).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchUrl]);
+  console.log(movies);
+  return (
+    <div>
+      <h2 className="p-4 font-bold text-white md:text-xl">{title}</h2>
+      <div className="relative flex items-center"></div>
+      <div id={"slider"}>
+        {movies.map((item, id) => (
+          <div className=""></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Main from '../Components/Main'
+import Row from '../Components/Row'
+import requests from "../HoldingReq";
+
 
 const Home = () => {
   return (
     <div>
         <Main></Main>
+        <Row title='Upcoming' fetchUrl={requests.requestUpcoming}></Row>
+        <Row title='Popular' fetchUrl={requests.requestPopular}></Row>
+        <Row title='Trending' fetchUrl={requests.requestTrending}></Row>
+        <Row title='Top Rated' fetchUrl={requests.requestTopRated}></Row>
+        <Row title='Horror' fetchUrl={requests.requestHorror}></Row>
+
     </div>
   )
 }
